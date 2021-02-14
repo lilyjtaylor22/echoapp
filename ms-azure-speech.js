fd355590a4efe6fee1937c414a0e4b698d44b7ed
lilyjtaylor22 <lilianajtaylor@stanford.edu>
2021-02-13 23:41:21 -0800

    ms-azure

---
+++ ms-azure-speech.js
@@ -0,0 +1,77 @@
+// this code does not work ... just cp/p from https://docs.microsoft.com/en-us/azure/cognitive-services/speech-service/speech-sdk?tabs=browser%2Cubuntu%2Cios-xcode%2Cmac-xcode%2Candroid-studio#get-the-speech-sdk
+// does contain the key, which is important ... but struggling to find how to get output???
+// methods documentation: https://docs.microsoft.com/en-us/javascript/api/microsoft-cognitiveservices-speech-sdk/speechconfig?view=azure-node-latest#enableDictation__
+// demo: https://azure.microsoft.com/en-us/services/cognitive-services/speech-to-text/#features
+
+// works in web browser apps
+// take my keeeeey ... signed up for free trial with Azure
+const sdk = require("microsoft-cognitiveservices-speech-sdk");
+const speechConfig = sdk.SpeechConfig.fromSubscription("68fa17c9-be19-4cb9-930b-f94780b70188", "West US 2");
+
+
+// configures microphone
+function fromMic() {
+    let audioConfig = sdk.AudioConfig.fromDefaultMicrophoneInput();
+    let recognizer = new sdk.SpeechRecognizer(speechConfig, audioConfig);
+    
+    console.log('Speak into your microphone.');
+    recognizer.recognizeOnceAsync(result => {
+        console.log(`RECOGNIZED: Text=${result.text}`);
+    });
+}
+fromMic();
+
+// change default from English
+speechConfig.speechRecognitionLanguage = "it-IT";
+
+
+// initialize speech recognition
+const recognizer = new sdk.SpeechRecognizer(speechConfig);
+
+// create events 
+recognizer.recognizing = (s, e) => {
+    console.log(`RECOGNIZING: Text=${e.result.text}`);
+};
+
+recognizer.recognized = (s, e) => {
+    if (e.result.reason == ResultReason.RecognizedSpeech) {
+        console.log(`RECOGNIZED: Text=${e.result.text}`);
+    }
+    else if (e.result.reason == ResultReason.NoMatch) {
+        console.log("NOMATCH: Speech could not be recognized.");
+    }
+};
+
+recognizer.canceled = (s, e) => {
+    console.log(`CANCELED: Reason=${e.reason}`);
+
+    if (e.reason == CancellationReason.Error) {
+        console.log(`"CANCELED: ErrorCode=${e.errorCode}`);
+        console.log(`"CANCELED: ErrorDetails=${e.errorDetails}`);
+        console.log("CANCELED: Did you update the subscription info?");
+    }
+
+    recognizer.stopContinuousRecognitionAsync();
+};
+
+recognizer.sessionStopped = (s, e) => {
+    console.log("\n    Session stopped event.");
+    recognizer.stopContinuousRecognitionAsync();
+};
+
+// start listening to speech
+recognizer.startContinuousRecognitionAsync();
+
+// make the following call at some point to stop recognition.
+// recognizer.StopContinuousRecognitionAsync();
+
+// output????
+
+
+// enables "period" --> "."
+// speechConfig.enableDictation();
+
+// can add dict of app specific vocab ayyyyy
+const phraseList = sdk.PhraseListGrammar.fromRecognizer(recognizer);
+phraseList.addPhrase("Supercalifragilisticexpialidocious");
+
